8239fa6f2afa0fe2f9b888d3028bf15338f90cf8
mrashed21 <rashedjaman768@gmail.com>
2025-11-17 00:34:35 +0600

    solved problem one

---
+++ solution.ts
@@ -0,0 +1,13 @@
+const formatValue = (value: string | number | boolean) => {
+  if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "boolean") {
+    return !value;
+  }
+};
+
+console.log(formatValue("hello"));
+console.log(formatValue(5));
+console.log(formatValue(true));
