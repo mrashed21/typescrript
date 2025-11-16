e6d43adea95b7bb58ec8c215e9c348c32d83a375
mrashed21 <rashedjaman768@gmail.com>
2025-11-17 00:48:03 +0600

    solved problem two

--- solution.ts
+++ solution.ts
@@ -11,3 +11,15 @@ const formatValue = (value: string | number | boolean) => {
 console.log(formatValue("hello"));
 console.log(formatValue(5));
 console.log(formatValue(true));
+
+const getLength = (value: string | Array<number>) => {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  if (Array.isArray(value)) {
+    return value.length;
+  }
+};
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
