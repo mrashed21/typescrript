94905b952449a7b25af280bd1c84a47c45e315e9
mrashed21 <rashedjaman768@gmail.com>
2025-11-17 01:57:32 +0600

    solved problem four

--- solution.ts
+++ solution.ts
@@ -42,3 +42,20 @@ console.log(person1.getDetails());
 
 const person2 = new Person("Alice", 25);
 console.log(person2.getDetails());
+
+interface FilterRating {
+  title: string;
+  rating: number;
+}
+
+const filterByRating = (books: Array<FilterRating>): Array<FilterRating> => {
+  return books.filter((book) => book.rating > 4);
+};
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+console.log(filterByRating(books));
